aa202c51fda189d9d24fa6ec766432dde3530777
tetete <tetetelego2013@gmail.com>
2018-12-02 21:17:45 +0900

    first commit

---
+++ AutoMaterialAssignSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "AutoAssignSettings", order = 1)]
+public class AutoMaterialAssignSettings : ScriptableObject {
+    public List<Material> materials;
+    public int slot;
+}
