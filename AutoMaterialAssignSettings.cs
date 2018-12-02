071a422e32a66c5ae3a88e84f2db11153dafeec8
tetete <tetetelego2013@gmail.com>
2018-12-02 21:16:48 +0900

    removed

--- AutoMaterialAssignSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "AutoAssignSettings", order = 1)]
-public class AutoMaterialAssignSettings : ScriptableObject {
-    public List<Material> materials;
-    public int slot;
-}
